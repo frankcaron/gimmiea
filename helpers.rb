e0e0b0f2d0b563bdbf9f2b03d969a895e15e466a
Frank Caron <frank@frankcaron.com>
2013-09-21 09:30:27 -0700

    Take 2, re: file path on Heroku

--- helpers.rb
+++ helpers.rb
@@ -35,16 +35,16 @@ end
 
 # This helper pics a random catch phrase.
 def pick_catchphrase
-    return File.readlines("catchphrases.txt").sample.tr("\n",)
+    return File.readlines(File.dirname(__FILE__) + "/catchphrases.txt").sample.tr("\n","")
 end
 
 # This helper pics a random slogan.
 def pick_slogan
-    return File.readlines("slogans.txt").sample.tr("\n",)
+    return File.readlines(File.dirname(__FILE__) + "/slogans.txt").sample.tr("\n","")
 end
 
 # This helper pics a random question for reset password requests.
 
 def pick_question
-    return File.readlines("questions.txt").sample.tr("\n",)
+    return File.readlines(File.dirname(__FILE__) + "/questions.txt").sample.tr("\n","")
 end
